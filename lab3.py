44c6df8354665952652d9f704d1145c287a54df9
jdman30 <drabekj1@southernct.edu>
2024-09-22 18:38:41 +0000

    lab 3 assignment 2

--- lab3.py
+++ lab3.py
@@ -1,1 +1,23 @@
 # lab 3
+
+#Objective:
+#Create a program that converts a temperature given in Celsius to Fahrenheit.
+
+#Instructions:
+
+#Prompt the user to enter the temperature in Celsius.
+#Use the conversion formula:
+#[ \text{Fahrenheit} = (\text{Celsius} \times 9/5) + 32 ]
+#Store the result in a variable.
+#Print the temperature in Fahrenheit.
+#Example Input/Output:
+
+#Enter the temperature in Celsius: 25
+#The temperature in Fahrenheit is: 77.0
+
+celsius = input ("Enter the temperature in Celsius: ")
+celsiustemp = float (celsius)
+
+fahrenheit = (celsiustemp * (9/5) + 32)
+
+print ("The temperature in Fahrenheit is: ", fahrenheit)
